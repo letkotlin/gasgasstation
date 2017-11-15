e452160cdd5db079757538ba77d78e2f2fca367b
kws <coreanim@helloworlds.co.kr>
2017-11-15 11:27:19 +0900

    add base icon, lib

--- app/src/main/java/com/gasgasstation/view/SplashActivity.kt
+++ app/src/main/java/com/gasgasstation/view/SplashActivity.kt
@@ -1,12 +1,13 @@
-package com.gasgasstation
+package com.gasgasstation.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.gasgasstation.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
