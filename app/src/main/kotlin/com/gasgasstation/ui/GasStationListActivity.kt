8a588051ebbadda7eb3d31ca8220b3ae6a660d19
KWS <coreanim@gmail.com>
2017-12-04 00:26:05 +0900

    현재 GPS 좌표 가져오기 추가 권한팝업 추가

--- app/src/main/kotlin/com/gasgasstation/ui/GasStationListActivity.kt
+++ app/src/main/kotlin/com/gasgasstation/ui/GasStationListActivity.kt
@@ -1,10 +1,21 @@
 package com.gasgasstation.ui
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import com.gasgasstation.R
 import com.gasgasstation.base.view.BaseActivity
+import com.gasgasstation.constant.Const
+
 
 class GasStationListActivity : BaseActivity() {
+
+    lateinit var locationManager: LocationManager
+
     override fun inject() {
 
     }
@@ -15,6 +26,21 @@ class GasStationListActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startLocationManager()
+    }
 
+    @SuppressLint("MissingPermission")
+    fun startLocationManager() {
+        locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                Log.i(Const.TAG, "GasStationListActivity latitude = " + location.latitude + " longitude = " + location.longitude)
+            }
+
+            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+            override fun onProviderEnabled(provider: String) {}
+            override fun onProviderDisabled(provider: String) {}
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
     }
 }
--- app/src/main/kotlin/com/gasgasstation/ui/SplashActivity.kt
+++ app/src/main/kotlin/com/gasgasstation/ui/SplashActivity.kt
@@ -1,10 +1,14 @@
 package com.gasgasstation.ui
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import com.gasgasstation.R
 import com.gasgasstation.base.view.BaseActivity
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import java.util.*
 
 class SplashActivity : BaseActivity() {
     override fun inject() {
@@ -17,14 +21,35 @@ class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        showPermission()
+        landingInitSetting()
+    }
 
+    fun showPermission() {
+        TedPermission(this)
+                .setPermissionListener(permissionlistener)
+                .setRationaleMessage(getString(R.string.auth_rationale_msg))
+                .setDeniedMessage(getString(R.string.auth_denied_msg))
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check()
+    }
+
+    internal var permissionlistener: PermissionListener = object : PermissionListener {
+        override fun onPermissionGranted() {
+
+        }
+
+        override fun onPermissionDenied(deniedPermissions: ArrayList<String>) {
+            showPermission()
+        }
+    }
+
+    fun landingInitSetting() {
         // 일정 시간 후에 화면 전환
         Handler().postDelayed({
             var intent = Intent(this, InitSettingActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
-
     }
-
 }
