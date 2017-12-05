1cbdf8b708449b7cb937fa4d2f6f33064eb04fa4
kws <coreanim@gmail.com>
2017-12-05 16:37:30 +0900

    rename package

--- app/src/main/kotlin/com/gasgasstation/model/enum/MapType.kt
+++
@@ -1,8 +0,0 @@
-package com.gasgasstation.model.enum
-
-/**
- * Created by kws on 2017. 11. 22..
- */
-enum class MapType(val map: String) {
-    GOOGLE("구글지도"), KAKAO("카카오네비"), APPLE("애플맵"), TMAP("티맵")
-}
--- app/src/main/kotlin/com/gasgasstation/model/enum/OrderType.kt
+++
@@ -1,8 +0,0 @@
-package com.gasgasstation.model.enum
-
-/**
- * Created by kws on 2017. 11. 22..
- */
-enum class OrderType(val order: String) {
-    DISTANCE("거리순"), PRICE("가격순")
-}
