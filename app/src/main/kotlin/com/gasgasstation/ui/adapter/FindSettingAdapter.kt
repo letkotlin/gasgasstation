15e2fde17026e301b4bdf9e03dc3b19418b731e0
KWS <coreanim@gmail.com>
2017-12-06 09:03:42 +0900

    add findsettingadapter

---
+++ app/src/main/kotlin/com/gasgasstation/ui/adapter/FindSettingAdapter.kt
@@ -0,0 +1,69 @@
+package com.gasgasstation.ui.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gasgasstation.R
+import com.gasgasstation.base.adapter.AdapterModel
+import com.gasgasstation.base.adapter.AdapterView
+import kotlinx.android.synthetic.main.item_init_setting.view.*
+
+/**
+ * Created by kws on 2017. 11. 30..
+ */
+class FindSettingAdapter(private val items: ArrayList<String> = ArrayList<String>()) : RecyclerView.Adapter<FindSettingAdapter.FindSettingViewHolder>(), AdapterModel<String>, AdapterView {
+
+    override fun onBindViewHolder(holder: FindSettingViewHolder?, position: Int) {
+        val item = getItem(position)
+        holder?.bind(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): FindSettingViewHolder {
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_init_setting, parent, false)
+        return FindSettingViewHolder(view)
+    }
+
+    override fun refresh() {
+        notifyDataSetChanged()
+    }
+
+    override fun addItems(items: List<String>) {
+        this.items.addAll(items)
+    }
+
+    override fun addItem(item: String) {
+        this.items.add(item)
+    }
+
+    override fun getItem(position: Int): String? {
+        if (position in 0..size()) {
+            return items[position]
+        } else {
+            return null
+        }
+    }
+
+    override fun clear() {
+        items.clear()
+    }
+
+    override fun size(): Int {
+        return itemCount
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class FindSettingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(item: String?) {
+            if (item == null)
+                return
+
+            itemView.tv_title.text = item
+        }
+    }
+
+}
